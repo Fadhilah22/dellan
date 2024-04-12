eb6e48d4383c755766e20edc18e338fad382e53f
Dellon Valentino Ardi <159220578+LenDlenDlen@users.noreply.github.com>
2024-04-12 17:21:25 +0800

    Update COMBINATION_MAGIC_LIBARY.py
    adding line to count runtime
    fix some issue with ECDSA algorithm 
    correcting ECDSA private key bit size 256 -> 384

--- combined_method/COMBINATION_MAGIC_LIBARY.py
+++ combined_method/COMBINATION_MAGIC_LIBARY.py
@@ -4,35 +4,40 @@ from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import utils
+import time
+
+start_keypair = time.time()
 
 # Generate RSA key pair
 rsa_private_key = rsa.generate_private_key(
     public_exponent=65537,
-    key_size=2048,
-    backend=default_backend()
+    key_size=2048
 )
 rsa_public_key = rsa_private_key.public_key()
 
 # Generate ECDSA key pair
 ec_private_key = ec.generate_private_key(
-    ec.SECP256R1(), 
-    default_backend()
+    ec.SEC384R1()
 )
 ec_public_key = ec_private_key.public_key()
+
+end_keypair = time.time()
+
+keypair_time = (end_keypair - start_keypair) * 1000
 
 # Message to be encrypted and signed
 message = b"The quick brown fox jumps over the lazy dog!"
 
+start_sign_n_enc = time.time()
+
 # Sign the message using ECDSA
-hash_message = hashes.Hash(hashes.SHA256(), backend=default_backend())
+hash_message = hashes.Hash(hashes.SHA256())
 hash_message.update(message)
 digest = hash_message.finalize()
 signature = ec_private_key.sign(
     digest,
     ec.ECDSA(utils.Prehashed(hashes.SHA256()))
 )
-
-print("This is the digital signature -> ", signature)
 
 # Combine digital signature and message into a string
 data_to_encrypt = signature + message
@@ -47,9 +52,8 @@ cipher_combined_data = rsa_public_key.encrypt(
     )
 )
 
-print ("This is cypherText -> ", cipher_combined_data)
-
-# Now you can send cipher_combined_data
+end_sign_n_enc = time.time()
+sign_enc = (end_sign_n_enc - start_sign_n_enc) * 1000
 
 # Decrypt the combined data using RSA
 decrypted_combined_data = rsa_private_key.decrypt(
